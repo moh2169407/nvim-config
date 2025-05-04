9aa89c5676ebd34522ef9a5070c7ea039453d103
Mohamud Mohamud <moh2169407@maricopa.edu>
2025-05-03 19:03:05 -0700

    Config nvim options

---
+++ lua/options.lua
@@ -0,0 +1,49 @@
+-- Sets line & relative numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+-- Sets copy & pasting to global clipboard
+vim.opt.clipboard = "unnamedplus"
+-- Sets long words that can't fit on a new line
+vim.opt.linebreak = true
+-- Sets spelling errors
+vim.opt.spell = true
+-- Sets default position for window when opening new files
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+-- Sets tabs as spaces instead
+vim.opt.expandtab = true
+-- Configs tab options
+vim.opt.softtabstop = 5
+vim.opt.shiftwidth = 5
+-- Sets the cursor in the middle
+vim.opt.scrolloff = 999
+
+vim.opt.wrap = false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Commnet
